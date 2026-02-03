ab06b763c25a1d74e336ce0a37f9f31a6be1d62b
Yuichi Yazaki <visualizingjp@gmail.com>
2026-02-03 22:33:46 +0900

    feat: add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KC67K4W9P1" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KC67K4W9P1');
+          `}
+        </Script>
         <Script src="/lib/supabase.js" strategy="beforeInteractive" />
         <Script src="/lib/dataviz-auth-client.js" strategy="afterInteractive" />
         {children}
